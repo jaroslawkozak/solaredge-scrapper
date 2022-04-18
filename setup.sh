53267a905b63a1d37af1fc39c40f3f598464c257
jaroslawkozak <41648123+jaroslawkozak@users.noreply.github.com>
2022-04-18 22:28:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ fi
 pip install influxdb-client
 cp solaredge_to_influx.service /lib/systemd/system/solaredge_to_influx.service
 systemctl daemon-reload
-systemctl enable shellscript.service
-systemctl start shellscript.service
+systemctl enable solaredge_to_influx.service
+systemctl start solaredge_to_influx.service
